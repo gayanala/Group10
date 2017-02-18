1ce2d4958674f80f770651d91ed5edb6419e01d3
Curtis Focht <curtisfocht@creighton.edu>
2017-02-17 23:18:33 -0600

    Getting imdb data

--- scraping/scrape_imdb.py
+++ scraping/scrape_imdb.py
@@ -1,13 +1,22 @@
 import imdb
+import csv
 
 ia = imdb.IMDb()
 
-series = ["Lost","The 100","Dexter","24","Smallville"]
+series = ["Lost","The 100","Dexter","24","Smallville","Veronica Mars","Prison Break","Heroes","Homeland","Nip/Tuck","True Blood"]
+with open("episode_ratings.csv","w") as outfile:
+    writer = csv.writer(outfile)
+    for s in series:
+        results = ia.search_movie(s)
+        movie_id = results[0].movieID
+        show = ia.get_movie(movie_id)
+        ia.update(show, "episodes")
+        episode_id = show['episodes']
+        for season in show['episodes']:
+            for episode in season:
+                episode = ia.get_movie(episode.movieID)
 
-for s in series:
-    results = ia.search_movie(s)
-    for result in results:
-        if result["kind"] == 'tv series':
-            print result
-            print result.movieID
-            break
+                writer.writerow([episode["rating"])
+
+
+
