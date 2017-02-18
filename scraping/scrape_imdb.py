d2dd28e7ee3ae328934dddc824ee01045fd0e2ae
Curtis Focht <curtisfocht@creighton.edu>
2017-02-18 00:17:32 -0600

    Added Results

--- scraping/scrape_imdb.py
+++ scraping/scrape_imdb.py
@@ -3,7 +3,8 @@ import csv
 
 ia = imdb.IMDb()
 
-series = ["Lost","The 100","Dexter","24","Smallville","Veronica Mars","Prison Break","Heroes","Homeland","Nip/Tuck","True Blood"]
+series = ["Lost", "The 100", "Dexter", "24", "Smallville", "Veronica Mars", "Prison Break", "Heroes", "Homeland",
+          "Nip/Tuck", "True Blood"]
 with open("episode_ratings.csv","w") as outfile:
     writer = csv.writer(outfile)
     for s in series:
@@ -11,12 +12,16 @@ with open("episode_ratings.csv","w") as outfile:
         movie_id = results[0].movieID
         show = ia.get_movie(movie_id)
         ia.update(show, "episodes")
-        episode_id = show['episodes']
-        for season in show['episodes']:
-            for episode in season:
+        for season_num, season in show['episodes'].items():
+            for episode_num,episode in season.items():
                 episode = ia.get_movie(episode.movieID)
-
-                writer.writerow([episode["rating"])
+                row = [season_num,episode_num]
+                for key in ["smart canonical series title", "smart canonical episode title", "rating", "vote", "year"]:
+                    try:
+                        row.append(episode[key])
+                    except KeyError:
+                        row.append("")
+                writer.writerow(row)
 
 
 
