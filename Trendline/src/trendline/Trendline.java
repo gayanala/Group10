4c28b6868345ff8d5e8e9db5de4ff57eb91849bb
cdr23859 <cdr23859@creighton.edu>
2017-02-18 08:46:53 -0600

    Produces Yield Warnings

--- Trendline/src/trendline/Trendline.java
+++ Trendline/src/trendline/Trendline.java
@@ -1,42 +1,45 @@
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.TreeSet;
 
 
 /**
- *
+ * Class which takes a file as input and produces useful measurements of popularity, expectations, and overall satisfaction
  * @author Group10
  */
 public class Trendline {
     private static ArrayList<VideoEvent> streamingVideo;
+    private static ArrayList<Double> expectationsChange;
+    private static final int yieldAfterNumNegative = 5;
     private static String Name;
     private static String FileName = "How I met your mother";
-    
+ 
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
         
         File file = new File(Trendline.FileName+".csv");
         PrintWriter writer = new PrintWriter(Trendline.FileName+"WithBonus.txt", "UTF-8");
         Trendline ShowTrend = new Trendline(file);
-        writer.write(Trendline.Name+"\n");
+        writer.write(Trendline.getYield()+"\n");
         
         for(int i = 0; i<Trendline.streamingVideo.size(); i++){
             VideoEvent show = Trendline.streamingVideo.get(i);
-            String print = show.toString() + Trendline.getExpectations(i)+", " + show.getNumVotes();
+            String print = show.toString() + Trendline.expectationsChange.get(i)+"\t" + show.getNumVotes();
             Trendline.getRunningAverageVoting(i);
             writer.write(print+"\n");
         }
-        
-        writer.close();
-        
+        writer.close();  
     }
-    
+    /**
+     * Constructs an Object of the Trendline Class (old name with new ideas)
+     * @param FileName the file to be read from
+     * @throws FileNotFoundException 
+     */
     public Trendline(File FileName) throws FileNotFoundException{
         Trendline.streamingVideo = new ArrayList();
+        Trendline.expectationsChange = new ArrayList();
         Scanner input = new Scanner(FileName);
         Trendline.Name = "";
         int order = 0;
@@ -48,52 +51,85 @@ public class Trendline {
             String NumEpisode = line[1];
             
             Trendline.Name = line[2];
+            
             String episodeName = line[3];
             String Rank = line[4];
             String NumVotes = line[5];
-            String Year = line[6];
  
-            VideoEvent show = new VideoEvent(order, NumSeason, NumEpisode, episodeName, Rank, NumVotes, Year);
-
+            VideoEvent show = new VideoEvent(order, NumSeason, NumEpisode, episodeName, Rank, NumVotes);
+            
             Trendline.streamingVideo.add(show);
+        }
+        for(int i = 0; i<Trendline.streamingVideo.size(); i++){
+            Trendline.expectationsChange.add(Trendline.getExpectations(i));
         }
         input.close();
     }
-    
+    /**
+     * Produces a running average of the viewer ranking until a specified VideoEvent's index
+     * @param ShowIndex the specified VideoEvent's index, the end of the run
+     * @return the average calculated as a double
+     */
     public static double getRunningAverageRating(int ShowIndex){
         double TotalRank = 0.0;
-        
         for(int i = 0; i<ShowIndex; i++){
-          
             VideoEvent show = Trendline.streamingVideo.get(i); 
-            TotalRank += show.getRank();
-           
+            TotalRank += show.getRank(); 
         }
         TotalRank = (double)Math.round(TotalRank*10)/10.0;
         return TotalRank/(1.00*(ShowIndex));
     }
-    
+    /**
+     * Produces a measure of viewers' expectations for a single VideoEvent based on the previous events
+     * @param ShowIndex the index of the VideoEvent
+     * @return the measure viewers' expectations
+     */
     public static double getExpectations(int ShowIndex){
         double expectedRank = 0.0;
-        System.out.println("CurrentRank "+Trendline.streamingVideo.get(ShowIndex).getRank()
-                    +" and running average is "+Trendline.getRunningAverageRating(ShowIndex));
         expectedRank = Trendline.streamingVideo.get(ShowIndex).getRank()
                     - Trendline.getRunningAverageRating(ShowIndex);
-        
-        
         return expectedRank;
     }
+    /**
+     * Produces a running average of the total viewer votes until a specified VideoEvent's index
+     * @param ShowIndex the specified VideoEvent's index
+     * @return the calculated average
+     */
+    public static double getRunningAverageVoting(int ShowIndex){
+       double TotalVotes = 0.0;
+
+       for(int i = 0; i<ShowIndex; i++){
+           VideoEvent show = Trendline.streamingVideo.get(i);  
+           TotalVotes += show.getNumVotes();
+       }
+
+       double rav = TotalVotes/(1.00*ShowIndex);
+       return rav;  
+    }
     
-     public static double getRunningAverageVoting(int ShowIndex){
-        double TotalVotes = 0.0;
-        
-        for(int i = 0; i<ShowIndex; i++){
-            VideoEvent show = Trendline.streamingVideo.get(i);  
-            TotalVotes += show.getNumVotes();
+    public static String getYield(){
+        int NumNegativeExpectations = 0;
+        int index = 0;
+        String yieldInfo = "#";
+        VideoEvent yield = null;
+        while(index<Trendline.expectationsChange.size()){
+            System.out.println("The index is: "+index+ " and the number of negatives are: "+NumNegativeExpectations);
+            if(Trendline.expectationsChange.get(index)<0){
+                NumNegativeExpectations++;
+            }
+            else{
+                NumNegativeExpectations = 0;
+                yield = null;
+            }
+            if(NumNegativeExpectations==Trendline.yieldAfterNumNegative){
+                yield = Trendline.streamingVideo.get(index-Trendline.yieldAfterNumNegative+1);
+                yieldInfo += yield.getIdentifier() + " "+ yield.getName()+"\t";
+                System.out.println("So I will add a yield");
+            }
+            index++;
         }
         
-        double rav = TotalVotes/(1.00*ShowIndex);
-        return rav;  
+        return yieldInfo;
     }
-
+    
 }
--- Trendline/src/trendline/VideoEvent.java
+++ Trendline/src/trendline/VideoEvent.java
@@ -1,6 +1,5 @@
-
 /**
- *
+ * Class which takes given data for a Video and produces a way of generating useful measurements
  * @author Group10
  */
 public class VideoEvent implements Comparable {
@@ -9,33 +8,53 @@ public class VideoEvent implements Comparable {
     private String Name;
     private String Rank;
     private String NumVotes;
-    private String Year;
-    
-    public VideoEvent(int ApperanceOrder, String SeasonNumber, String EpisodeNumber, String Name, String Rank, String Votes, String Year){
+    /**
+     * Constructs on object of the VideoEvent Class
+     * @param ApperanceOrder the location within a series
+     * @param SeasonNumber the season number
+     * @param EpisodeNumber the event's location within a season
+     * @param Name the event's name
+     * @param Rank viewer given rank (0.0-10.0)
+     * @param Votes total number of viewer votes
+     */
+    public VideoEvent(int ApperanceOrder, String SeasonNumber, String EpisodeNumber, String Name, String Rank, String Votes){
         this.NumAppeared = ApperanceOrder;
         this.Identifier = "S"+SeasonNumber+":E"+EpisodeNumber;
         this.Name = Name;
         this.Rank = Rank;
         this.NumVotes = Votes;
-        this.Year = Year;
         
     }
-    
+    /**
+     * Provides access to the event's identifying locations
+     * @return the identifying locations (i.e. S2:E5)
+     */
     public String getIdentifier(){
         return new String(this.Identifier);
     }
-    
+    /**
+     * Provides access to the event's viewer given rank
+     * @return the viewer ranking as a double (within 0.0-10.0)
+     */
     public double getRank(){
         return Double.parseDouble(this.Rank);
     }
-    
+    /**
+     * Provides access to the event's total votes (the current representation of total views)
+     * @return the total number of votes as an integer (scaled by a factor of 10^3)
+     */
     public double getNumVotes(){
         return Integer.valueOf(this.NumVotes)/1000.0;
     }
     
+    public String getName(){
+        return new String(this.Name);
+    }
+    /**
+     * Produces a measurement of total views using both viewer rank and total votes
+     * @return a String representation of our Rank/Vote view measurement (scaled by a factor of 10^4)
+     */
     public String getWatchedIndicator(){
-        //possible to change based on Stastical evidence of correlation 
-        //(for now our only indicator is Viewer Ranking and Number of Votes)
         if(this.Rank.equals("")||this.NumVotes.equals("")){
             return "No given data";
         }
@@ -43,15 +62,15 @@ public class VideoEvent implements Comparable {
     }
    
     @Override
-     public String toString(){
-        String print = this.NumAppeared+", ";
+    public String toString(){
+        String print = this.NumAppeared+"\t";
         if(this.Name.equals("")){
-            print += this.Identifier+"No Name Given, ";
+            print += this.Identifier+" No Name Given\t";
         }
         else{
-            print += "'"+this.Identifier+" "+this.Name+"'"+", ";
+            print += "\""+this.Identifier+" "+this.Name+"\""+"\t";
         }
-        print += this.Rank+", ";
+        print += this.Rank+"\t";
         
         return print;
     }
